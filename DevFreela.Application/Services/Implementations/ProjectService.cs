fda66128d96c5f1faec8cceb2467962e8a66dd90
Mainoí Neumann Estivalet <mainoi.estivalet@sequor.com.br>
2023-11-10 09:55:37 -0300

    Application layer 1

---
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class ProjectService : IProjectService
+    {
+        public int Create(NewProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateComment(CreateCommentInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetAll(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectDetailsViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+
+        ProjectDetailsViewModel GetById(int id);
+
+        int Create(NewProjectInputModel inputModel);
+
+        void Update(UpdateProjectInputModel inputModel);
+
+        void Delete(int id);
+
+        void Start(int id);
+
+        void Finish(int id);
+
+        void CreateComment(CreateCommentInputModel inputModel);
+    }
+}
